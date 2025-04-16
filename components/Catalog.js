54cad5348192cc8947eec37730bbcad6ffa41dd5
mvinicius13 <michael.vinicius@levelmicro.com.br>
2025-04-16 16:32:00 -0300

    Update Catalog.js

--- components/Catalog.js
+++ components/Catalog.js
@@ -174,6 +174,20 @@ return (
         As imagens são meramente ilustrativas e foram obtidas automaticamente por pesquisa no Google.
       </footer>
 
+      {/* Botão flutuante para baixar Excel */}
+      <a
+        href="https://docs.google.com/spreadsheets/d/1FQRXOr27B1N7PK7NhqQmPi1kaqQqImA-iZYjRecqIw0/export?format=xlsx"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed bottom-24 right-5 bg-green-700 hover:bg-green-800 text-white p-3 rounded-full shadow-lg transition"
+        title="Baixar catálogo em Excel"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 fill-current" viewBox="0 0 24 24">
+          <path d="M19 2H8a2 2 0 00-2 2v2H5a2 2 0 00-2 2v12a2 2 0 002 2h11a2 2 0 002-2v-2h1a2 2 0 002-2V4a2 2 0 00-2-2zM5 8h1v10H5V8zm13 10a1 1 0 01-1 1H8a1 1 0 01-1-1V4h11v14zm3-4a1 1 0 01-1 1h-1V8h1a1 1 0 011 1v5z"/>
+          <path d="M10.293 12l1.853 1.854a.5.5 0 01-.707.707L9.586 12l1.853-1.854a.5.5 0 11.707.707L10.293 12z"/>
+        </svg>
+      </a>
+
       {/* Botão flutuante do WhatsApp */}
       <a
         href="https://wa.me/5511994448143"
